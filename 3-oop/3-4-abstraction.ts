81daae862cafea6f4c04b7805094f08d26ed07ee
vanillovin <vanillovin@gmail.com>
2023-02-05 23:28:25 +0900

    add: abstraction - interface

---
+++ 3-oop/3-4-abstraction.ts
@@ -0,0 +1,77 @@
+{
+  /**
+   * 추상화하는 방법
+   * 1. interface 규약 계약서/명세서
+   */
+
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('cleaning the machine...🧼');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up...🔥');
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots...☕`);
+      return { shots, hasMilk: false };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+  maker.makeCoffee(2);
+
+  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  // maker2.fillCoffeeBeans(32); CoffeeMaker는 makeCoffee 밖에 없음.
+  // fillCoffeeBeans api는 CoffeeMaker interface에 존재하지 않음. 사용 x
+  maker2.makeCoffee(2);
+}
